e1fa674995fc45518c1978460cbbe315db032cef
Kamandlou <ahkamandlou@gmail.com>
2023-12-12 16:53:51 +0330

    Initial commit

---
+++ src/pf1/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "pf1"
+__description__ = "simple Python package"
+__url__ = "https://github.com/kamandlou/pf1"
+__version__ = "0.1.0"
+__license__ = "Apache License 2.0"
+__author__ = "Amir Hossein Kamandlou"
+__email__ = "ahkamandlou@gmail.com"
+
